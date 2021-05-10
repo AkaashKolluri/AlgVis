5373835bb6fe03c559b78d79a699edf710baed32
Akaash Kolluri <akaashkolluri@akaashs-air.lan>
2021-05-10 12:20:54 -0500

    set up intial j frame + tests

---
+++ HomeScreen.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+
+
+
+public class HomeScreen extends JPanel{
+    
+    HomeScreen()
+	{
+        //setting size to whatever the window size is, and making it visible
+		setSize(Main.width, Main.height);
+		setVisible(true);
+	}
+	
+	public void paintComponent(Graphics g)
+	{
+        //should draw a black rectangle with 100 border 
+		g.setColor(Color.BLACK);
+		g.fillRect(100, 100, Main.width-200, Main.height-200);
+		
+	}
+}
--- Main.java
+++ Main.java
@@ -1,8 +1,25 @@
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+
+
 public class Main
 {
+    //setting width and height variables as public so we can acess them from other files to resize the screen based on screen width and height
+    public static int width = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().width;
+    public static int height = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().height;
+    public static void main(String[] args) 
+    {
+        //creating frame
+        JFrame j = new JFrame(); 
+        //adding home screen and setting size
+        HomeScreen h = new HomeScreen();
+        j.setSize(width, height);
+        j.add(h);
+        //making the frame be seen and exitable
+        j.setVisible(true);
+		j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+    }
 
 
-public static void main(String[] args) {
-    
-}
 }
